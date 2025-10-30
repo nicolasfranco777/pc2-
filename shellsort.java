27ac28edbfe20cf4ef4919a7abce41d198c932f3
nicolasfranco777 <nf702136@gmail.com>
2025-10-30 09:43:03 -0500

    Fix indentation and formatting in shellsort.java

--- shellsort.java
+++ shellsort.java
@@ -11,8 +11,8 @@ public class shellsort {
                 while (j >= gap && arr[j - gap] > temp) {
                     arr[j] = arr[j - gap];
                     j -= gap;
-                }
-                arr[j] = temp;             
+                } 
+                arr[j] = temp;                 
             }       
         }
     }
